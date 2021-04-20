ffe3c4a3905c2d67ac0096d2a5b2a52335410083
Marinelic <75620592+Marinelic@users.noreply.github.com>
2021-04-20 19:20:18 +0200

    doing changes, practicing git

--- functions.php
+++ functions.php
@@ -4,17 +4,14 @@ function university_files() {
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   
-  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=yourkeygoeshere', NULL, '1.0', true);
-  wp_enqueue_script('axios', '//cdn.jsdelivr.net/npm/axios/dist/axios.min.js', NULL, '1.0', true);
-  wp_enqueue_script('glidejs', '//cdn.jsdelivr.net/npm/@glidejs/glide', NULL, '1.0', true);
+  if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
+    wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+  } else {
+    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.24d997ebbc93d4ed3bb9.js'), NULL, '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.f7a04b38ed77f26dd44c.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.f7a04b38ed77f26dd44c.css'));
+  }
 
-  wp_enqueue_script('main-university-js', get_theme_file_uri('/scripts.js'), array('jquery'), '1.0', true);
-  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
-
-  wp_localize_script('main-university-js', 'universityData', array(
-    'root_url' => get_site_url(),
-    'nonce' => wp_create_nonce('wp_rest')
-  ));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');

cb1bf5854226b0fb415830df9776dd4a1a6e16ae
Marinelic <75620592+Marinelic@users.noreply.github.com>
2021-05-08 17:31:28 +0200

    Synchronous vs Asynchronous

--- functions.php
+++ functions.php
@@ -38,8 +38,8 @@ function university_files() {
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
     wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.b2903539c78f3822bcf5.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.ccce486d289f107e3b19.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.ccce486d289f107e3b19.css'));
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.53bd07ec94f3e092aa4e.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.53bd07ec94f3e092aa4e.css'));
   }
 
   wp_localize_script('main-university-js', 'universityData', array(
